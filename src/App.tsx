dabdd0fb368dc6b01aa746642904248c2441e635
chian lei so <chianlei.so@atoz-software.tech>
2022-11-16 14:32:06 +0800

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
 
 function App() {
+
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Lei" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
